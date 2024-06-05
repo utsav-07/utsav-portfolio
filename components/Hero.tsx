2ddb77d7f9ad2e3a5c112f3cb5eff5ba404baa64
utsav-07 <utsavudit07@gmail.com>
2024-06-06 03:17:37 +0530

    text-chnage

--- components/Hero.tsx
+++ components/Hero.tsx
@@ -43,7 +43,7 @@ const Hero = () => {
           />
 
           <p className="text-center md:tracking-wider mb-4 text-sm md:text-lg lg:text-2xl">
-            Hi! I&apos;m utsav, a Next.js Developer.
+            Hi! I&apos;m utsav, a  Developer.
           </p>
 
           <a href ="#about">
